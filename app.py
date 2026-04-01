add02ac5084997239dd54405b12862e6f38b0027
sriram <sriramcj2426@gmail.com>
2026-04-01 10:18:02 +0000

    level 2 pipeline

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
-from flask import Flask, request
-import os
-
-app = Flask(__name__)
-
-@app.route("/api")
-def home():
-    name = request.args.get("name", "Guest")
-    message = os.getenv("MESSAGE", "hello")
-    return f"{message}, {name}!"
-
-app.run(host="0.0.0.0", port=5000)
+print("starting the app")
+print("CI/CD level 2")
